3a4727d3437f99ffd605900c243a03b04465fb14
Danusha U <danuu@qti.qualcomm.com>
2023-03-16 21:54:49 +0530

    npm usage

---
+++ npm_usage/test.mjs
@@ -0,0 +1,10 @@
+// use EGMA rule
+// var cowsay = require("cowsay");
+import cowsay from "cowsay";
+console.log(
+  cowsay.say({
+    text: "My name is Pinkyyyy",
+    e: "oO",
+    T: "U ",
+  })
+);
